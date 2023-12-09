8e14f339c599d2580978d3ebb221181b719661a6
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2023-12-09 15:40:49 +0300

    Initial Commit

---
+++ Calculator_ver3/Calculator_ver3App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_ver3App.swift
+//  Calculator_ver3
+//
+//  Created by MIKHAIL ZHACHKO on 9.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_ver3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
